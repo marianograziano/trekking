dcbf2895bd092eb35c1d1e674c548d251228440e
marianograziano <mariano.graziano@gmail.com>
2024-05-20 13:46:09 -0300

    restarting

--- scriptsjs.php
+++
@@ -1,6 +0,0 @@
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.5.1/gpx.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="./admin/dist/js/adminlte.min.js"></script>
--- stylescss.php
+++
@@ -1,5 +0,0 @@
-
-<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="./assets/css/trek.css" />
-<link href="./admin/dist/css/adminlte.min.css" rel="stylesheet">
